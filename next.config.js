b4d89fd58914a638ee48d0086112fb25d9f508cd
wesleywil <wesleywilsonti@gmail.com>
2023-06-27 22:46:43 -0300

    updated next.config.js by including imgur hostname to the remotePatterns configuration

--- next.config.js
+++ next.config.js
@@ -1,16 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {
-                protocol:'https',
-                hostname:'lh3.googleusercontent.com/**',
-            }
-        ]
-    },
-    experimental:{
-        serverActions:true,
-    }
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.imgur.com/**",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
